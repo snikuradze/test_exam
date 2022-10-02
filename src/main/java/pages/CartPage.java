eb7e4797a81ab8438f37e2467d84d752790ac7a5
nikuradze <sergey.nikuradze@gmail.com>
2022-10-02 17:54:37 +0300

    additional fix

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -28,7 +28,7 @@ public class CartPage extends ParentPage{
     }
     public CartPage checkCorrectProductIsAdded(int productIndex){
         ProductPage productPage = new ProductPage(webDriver);
-        Assert.assertEquals("Incorrect product is displayed", productPage.buttonRemove.get(productIndex).getAttribute("name"), buttonRemove.get(productIndex).getAttribute("name"));
+        Assert.assertEquals("Incorrect product is displayed", productPage.buttonRemove.get(productIndex).getAttribute("name").substring(7), buttonRemove.get(productIndex).getAttribute("name").substring(7));
         logger.info(buttonRemove.get(productIndex).getAttribute("name").substring(7) + " product is added");
         return this;
     }
